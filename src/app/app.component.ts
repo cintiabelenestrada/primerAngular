40e4b2ad2b3c1912f4deddcfb9aa54b9a84c9ea0
CintiaBelenEstrada <cintiabelenestrada@gmail.com>
2024-05-08 13:37:48 -0300

    Uso de un Componente dentro del componente principal app-root

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet,UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  // otra forma de hacerlo seria poniendo los elementos que quiero mostrar,
+  //  pero ahora moveria todo este h1 con el user al hmlt de rootapp
+  // template: `<h1>Hola ciudad de {{city.toUpperCase()}}</h1>
+  //   <app-user></app-user>`,
+  styles: '',
 })
+
 export class AppComponent {
-  title = 'primerAngular';
+  city = 'Jujuy!';
 }
